a0914c12574f4bc4d25a89092e7cfde1a260fc1b
silverashiq <silverashiq@gmail.com>
2024-06-30 03:26:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const searchInput = document.getElementById("searchInput")
+
+const searchIcon = document.getElementById("searchIcon")    
+
+const weatherIcon = document.getElementById("weatherIcon")   
+
+const myTemp = document.getElementById("myTemp")   
+
+const myDetails = document.getElementById("myDetails")   
+
+const myDate = document.getElementById("myDate")   
+
+const myCountry = document.getElementById("myCountry")  
+
+const myTime = document.getElementById("myTime")  
+
+const loadParent = document.getElementById("loadParent") 
+
+const boxQ1 = document.getElementById("boxQ1") 
+
+const boxA1 = document.getElementById("boxA1") 
+
+const myApi = "9d207f736d19d98f87e37da86a7d9b40"
