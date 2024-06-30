8570bc5f4d69bb42b0a4a3f23691b20c6d3f3025
silverashiq <silverashiq@gmail.com>
2024-06-30 21:10:25 +0600

    almost done

--- app.js
+++ app.js
@@ -51,7 +51,39 @@ const showLoad= ()=>{
     loadParent.innerHTML=designLoad;
 }
 
-
+async function searchLocation(){
+    const locationText=searchInput.value.trim();
+    if(locationText==""){
+        alert("Please Enter a City Name")
+        return;
+    }
+     if(location){
+        showLoad();
+        const apiResponse2= await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${locationText}&appid=${myKey}&units=metric` );
+        const apiData2=await apiResponse2.json();
+        console.log(apiData2)
+        const myIcon2 = '<img class="size-[250px]" src="https://openweathermap.org/img/wn/09n@4x.png" alt="Icon" id="myIcon">'
+        weatherIcon.innerHTML=myIcon2
+        myTemp.innerText=Math.round(apiData2.main.temp) + "°C";
+        myDetails.innerText=apiData2.weather[0].description
+        myFeel.innerText=("Feels like - " + apiData2.main.feels_like + "°C") 
+        myCloud.innerText=("Clouds - " + apiData2.clouds.all   )
+        myArea.innerText=(apiData2.name + ",")
+        myCountry.innerText=(apiData2.sys.country)
+        loadParent.classList.add('hidden');
+        mainDisplay.classList.remove('hidden');
+    
+        myBox1.innerText=(apiData2.weather[0].main)
+        myBox2.innerText=(apiData2.main.temp_min)+"°C"
+        myBox3.innerText=(apiData2.main.temp_max)+"°C"
+        myBox4.innerText=apiData2.timezone
+        myBox5.innerText=(apiData2.wind.speed)+"m/s"
+        myBox6.innerText=(apiData2.wind.deg)+"°"
+        myBox7.innerText=apiData2.main.pressure
+        myBox8.innerText=apiData2.main.humidity
+        myBox9.innerText=apiData2.main.sea_level
+     }
+}
 
 
 // User's Weather from API
@@ -62,8 +94,6 @@ async function mainWeather(lat, long){
     // Direct link https://api.openweathermap.org/data/2.5/weather?lat=23.7278846&lon=90.3654296&appid=9d207f736d19d98f87e37da86a7d9b40&units=metric
 
     const apiData = await apiResponse.json();
-
-    console.log(apiData)
 
 
     
@@ -120,19 +150,5 @@ if(navigator.geolocation) {
     } )
 }
 
+searchBtn.addEventListener('click', searchLocation)
 
-// function async function searchLocation(){
-//     const locationText=searchInput.value.trim();
-//     if (locationText == "" ){
-//     alert("Please Enter Location")
-//     return;
-//     }
-//     if(location){
-//         showLoad();
-//         const apiResponse = await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${locationText}&appid=${myKey}` );
-
-//         const apiData=await apiResponse.json();
-    
-        
-
-//     }
